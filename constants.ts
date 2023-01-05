12d18246f1b54328c639f95c1ec0085b86c5bb01
Ricci Mwangela <riccimwangela@gmail.com>
2023-01-05 00:53:42 +0000

    constants

---
+++ constants.ts
@@ -0,0 +1,188 @@
+export const EXPERIENCE = {
+  HEADING:
+    'We aim to build an incredible experience for ordering custom products.',
+  DESCRIPTION:
+    'Wasting time sucks. That’s why we relentlessly focus on making it fast and easy to order custom products. Order in seconds and get your products in days. Free proofs, free artwork help, free shipping and fast turnaround are why people love us.',
+  IMAGESRC:
+    'https://assets.stickermule.com/image/upload/c_lfill,fl_lossy,f_auto,q_auto:best,w_auto/re-store-shared/about-experience-image.jpg',
+}
+
+export const TEAM = {
+  HEADING: 'We are powered by a remote team across 17 countries.',
+  DESCRIPTION:
+    'Our team comes from diverse backgrounds and 17 countries, but we’re all united by a shared set of values. We happily work from the United States, Argentina, Bosnia & Herzegovina, Brazil, Canada, Croatia, Finland, France, Germany, Guatemala, India, Italy, Poland, Slovakia, Spain, Sweden, and the UK.',
+}
+
+export const CUSTOMERS = {
+  HEADING: 'We ship to awesome customers worldwide.',
+  DESCRIPTION:
+    'Each year we ship worldwide to thousands of customers in 70+ countries. Along with available free worldwide shipping, we support 16 unique languages and 7 currencies (ARS, AUD, CAD, EUR, GBP, MXN, and USD).',
+  IMAGESRC:
+    'https://assets.stickermule.com/image/upload/c_lfill,fl_lossy,f_auto,q_auto:best,w_auto/re-store-shared/about-customers-image.jpg',
+}
+
+export const FOOTER = [
+  {
+    title: 'Products',
+    Links: [
+      'Stickers',
+      'Labels',
+      'Magnets',
+      'Buttons',
+      'Packaging',
+      'T-Shirts',
+      'More',
+    ],
+  },
+  {
+    title: 'Tools',
+    Links: ['Studio', 'Trace', 'Upscale', 'Redraw'],
+  },
+  {
+    title: 'Company',
+    Links: ['About', 'Blog', 'Careers', 'Press'],
+  },
+  {
+    title: 'Resources',
+    Links: ['Deals', 'Templates', 'Teams', 'Uses', 'Quick order'],
+  },
+  {
+    title: 'Support',
+    Links: ['Help', 'Returns'],
+  },
+]
+
+type Location = {
+  name: string,
+  latitude: number,
+  longitude: number,
+  image: string,
+}
+export const LOCATIONS: Location[] = [
+  {
+    name: 'Toni Bauch',
+    latitude: 40.7128,
+    longitude: -73.9385,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Arlene Orn',
+    latitude: 34.0522,
+    longitude: -118.2437,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Brendan Rohan',
+    latitude: 41.8781,
+    longitude: -87.6298,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Delores Kling',
+    latitude: 29.7604,
+    longitude: -95.3698,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Saul Bode Sr.',
+    latitude: 33.4484,
+    longitude: -112.074,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Tony Waters',
+    latitude: 39.9526,
+    longitude: -75.1652,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Betty Reichert',
+    latitude: 29.4241,
+    longitude: -98.4936,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Emmett Rosenbaum',
+    latitude: 32.7157,
+    longitude: -117.1611,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Michelle Stracke',
+    latitude: 32.7767,
+    longitude: -96.797,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Virgil Lockman',
+    latitude: 37.3382,
+    longitude: -121.8863,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Lila Kuhlman',
+    latitude: 30.2672,
+    longitude: -97.7431,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Willard Harber',
+    latitude: 30.3322,
+    longitude: -81.6557,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Jeff Zemlak',
+    latitude: 32.7555,
+    longitude: -97.3308,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Dr. Winifred Ankunding',
+    latitude: 39.9612,
+    longitude: -82.9988,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Rosa Rodriguez',
+    latitude: 37.7749,
+    longitude: -122.4194,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Karl Jerde',
+    latitude: 35.2271,
+    longitude: -80.8431,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Hannah Barrows',
+    latitude: 39.7684,
+    longitude: -86.1581,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+  {
+    name: 'Emma Stone',
+    latitude: 47.6062,
+    longitude: -122.3321,
+    image:
+      'https://assets.stickermule.com/image/fetch/ar_1,c_limit,fl_lossy,f_auto,q_auto:best,w_179/https%3A%2F%2Fstorage.googleapis.com%2Fsm-studio%2Fstudio%2F2baaedac-b7ec-4de6-9ee3-870369ca1bdf-published.png%3Ff%3Ddc21f2b357fccf0f6d994ebc476f12f2',
+  },
+]
